440416dc82cd5415fa4a63ab17a4ca308f168a73
Saulius-Saulys <sauliusaulys@gmail.com>
2021-01-16 19:08:22 +0200

    add migrations

---
+++ Api/database/migrations/2021_01_16_163707_create_offer_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfferTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offer', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('industry_id');
+            $table->foreign('industry_id')->references('id')->on('industry');
+            $table->string('description');
+            $table->string('employment_type')->nullable();
+            $table->float('salary_from');
+            $table->float('salary_to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offer');
+    }
+}
